a6a295a3497d79d740de9087aa8ae345e3ee00d5
JuanValdez <juantvaldez85@gmail.com>
2022-01-26 20:04:00 -0500

    completed Active Record exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,41 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+
+  
+
+
+  attr_accessor :name, :annual_revenue, :mens_apparel, :womens_apparel
+  
+  def initialize(name, annual_revenue, mens_apparel, womens_apparel)
+
+    @name = name
+    @annual_revenue = annual_revenue
+    @mens_apparel = true
+    @womens_apparel = true
+  end
+
+end
+
+
+
+store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+
+store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+
+store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+
+puts store1.name, store1.annual_revenue, store1.mens_apparel, store1.womens_apparel
+
+puts store2.name, store2.annual_revenue, store2.mens_apparel, store2.womens_apparel
+
+puts store3.name, store3.annual_revenue, store3.mens_apparel, store3.womens_apparel
+
+puts Store.count
+
+
