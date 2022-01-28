512aa2f0a87d487a8dc698dd953d57b38bfbe640
JuanValdez <juantvaldez85@gmail.com>
2022-01-27 21:57:51 -0500

    completed Active Record exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,21 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+
+puts "Total revenue of all stores: $#{@total_revenue}"
+
+@average_annual_income = @total_revenue / Store.count
+
+puts "Average annual revenue of all stores $#{@average_annual_income}"
+
+@stores_generating_over_million = Store.where("annual_revenue > '1000000'")
+
+@stores_generating_over_million.each do |store| 
+  
+  puts "Stores generating over $1,000,000: #{store.name}"
+ 
+end
+
+puts "Number of stores generating over $1,000,000: #{@stores_generating_over_million.size}"
+
